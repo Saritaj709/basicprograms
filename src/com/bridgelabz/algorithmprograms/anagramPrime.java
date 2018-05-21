3773afbf735d7beeba8c92b3b936661467cb8a93
Saritaj709 <Saritaj709@gmail.com>
2018-05-21 09:18:01 +0530

    algorithm pgm

--- src/com/bridgelabz/algorithmprograms/anagramPrime.java
+++ src/com/bridgelabz/algorithmprograms/anagramPrime.java
@@ -1,6 +1,6 @@
 /******************************************************************************
- *  Compilation:  javac -d bin primeNumbers.java
- *  Execution:    java -cp bin com.bridgelabz.util.primeNumbers.java 0 to 1000
+ *  Compilation:  javac -d bin anagramPrime.java
+ *  Execution:    java -cp bin com.bridgelabz.util.anagramPrime.java  0 to 1000
  *  Purpose: Detects if two strings are anagrams or not
  *
  *  @author  Sarita
@@ -10,13 +10,13 @@
  ******************************************************************************/
 
 
-
 package com.bridgelabz.algorithmprograms;
-import  com.bridgelabz.utility.*;
 
-public class primeNumbers {
+public class anagramPrime {
+	
 public static void main(String[] args)
 {
-	Utility.primeNumbers();
+	
 }
+
 }
